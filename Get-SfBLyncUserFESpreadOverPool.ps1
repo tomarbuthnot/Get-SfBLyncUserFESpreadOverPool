e0917274a66d2ef928e9c3580647cb58f6213e94
Tom Arbuthnot <tom@tomarbuthnot.com>
2015-11-04 12:49:03 -0800

    First Release
    A little scruffy but does the job

---
+++ Get-SfBLyncUserFESpreadOverPool.ps1
@@ -0,0 +1,59 @@
+﻿
+
+# Show how many and what percentage of users in a pool are assigned each FE as their primary server
+# This is defined by Lync Server via a hash vaue
+# Tom Arbuthnot
+# Chris Irons
+# Use entirely at your own risk
+
+# Instructions
+# Complete PoolName variable, run on a Front End Server
+# Will only work for an Enterprise Edition Pool
+
+
+# User Defined Variables
+$Poolname = "MK-SFB-FEPool01.domain.int"
+
+
+
+
+# Script
+$FEs = Get-CsPool -Identity $Poolname | Select Computers -ExpandProperty Computers
+
+$OutputCollection=  @()
+
+Get-CsUser -Filter {RegistrarPool -eq $Poolname} | ForEach-Object {
+                            $user = $_.SipAddress
+                            $FEOrder = $_ | Get-CsUserPoolInfo | Select-Object -ExpandProperty PrimaryPoolMachinesInPreferredOrder
+
+
+                            $output = New-Object -TypeName PSobject 
+                            $output | add-member NoteProperty "SIPAddress" -value $user
+                            $output | add-member NoteProperty "FEFirst" -value $FEOrder[0]
+                            $output | add-member NoteProperty "FESecond" -value $FEOrder[1]
+                            $output | add-member NoteProperty "FEThird" -value $FEOrder[2]
+                            
+                            $OutputCollection += $output
+                            }
+                
+$totalUsers = (Get-CsUser | Where-Object{$_.RegistrarPool -like $Poolname}).count
+
+
+$OutputCollection2 =  @()
+
+foreach ($FE in $FEs)
+{
+    
+    $totalperFE = ($OutputCollection | Where-Object {$_.FEFirst -eq $FE}).count
+    $FEPercentage = ($totalperFE/$totalUsers * 100)
+
+                            
+                            $output = New-Object -TypeName PSobject 
+                            $output | add-member NoteProperty "Pool" -value $Poolname
+                            $output | add-member NoteProperty "FE" -value $fe
+                            $output | add-member NoteProperty "Users" -value $totalperFE
+                            $output | add-member NoteProperty "Percentage" -value $FEPercentage
+                            $OutputCollection2 += $output
+
+}
+$OutputCollection2
